0789559a48a3c343d2e4d8c2bd7573dd239235a2
Tolik Punkoff <tolikgit@inbox.lv>
2021-08-03 10:13:19 +0300

    add console test app

---
+++ tests/testapp/src/testapp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            while (true)
+            {
+                int n = rnd.Next(0, int.MaxValue);
+                string t = Convert.ToString(n, 16);
+                Console.WriteLine(t.ToUpperInvariant());
+                System.Threading.Thread.Sleep(250);
+            }
+        }
+    }
+}
